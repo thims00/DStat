ba25b3174da5bb184b0d513c78012cbddb86147c
Tomm Smith <root.packet@gmail.com>
2014-02-07 22:51:38 -0500

    Introduced CLI arguments and "bagan" code for ANSI color handling.

--- dstat.py
+++ dstat.py
@@ -2,13 +2,17 @@
 
 
 import os
+import sys
 import psutil
 import time
+
+
 
 
 # Character width of progress bars
 bar_width = 12
 update_invl = 1
+stdout = False
 
 def mk_prog_bar(perc_val):
     bar_str = ""
@@ -39,6 +43,32 @@ def cpu_avg(cpu_loads):
     return avg
 
 
+def help():
+    print sys.argv[0] + " - A simple statusbar generator for dwm."
+    print """    -h,--help     - Print help and exit.
+    -n,--no-color - Do not include ANSI colors with the output.
+    -s,--stdout   - Print the bars to stdout and exit."""
+    
+    sys.exit(1)
+
+
+
+
+# Deal with our command line arguments
+for arg in sys.argv[1:]:
+    if arg == "-h" or arg == "--help":
+        help()
+
+    elif arg == "-n" or arg == "--no-color":
+        print "no color, or code"
+
+    elif arg == "-s" or arg == "--stdout":
+        stdout = True 
+    
+    else:
+        help()
+
+
 while True:
     cpu_perc = round(cpu_avg(psutil.cpu_percent(None, True)), 1)
     mem_perc = psutil.phymem_usage()[3]
@@ -49,6 +79,16 @@ while True:
     stats = "CPU " + str(cpu_perc) + cpu_bar + " "
     stats += "MEM " + str(mem_perc) + mem_bar
 
-    
-    ret = os.system("xsetroot -name '" + stats + "'")
+   
+    if stdout == True:
+        print(stats)
+        ret = 0
+        sys.exit(0)
+
+    else:
+        ret = os.system("xsetroot -name '" + stats + "'")
+        if ret > 0:
+            print("ERROR: Binary 'xsetroot' is not installed.")
+            sys.exit(255)
+
     time.sleep(update_invl)
