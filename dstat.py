3738bcd5e4804fa3f0368aaa7092d68e920be666
thims <root.packet@gmail.com>
2014-10-23 02:51:33 -0400

    Finished introducing the core code for a setup() and cleanup() of the IPC environment, and lead towards daemonization.

--- dstat.py
+++ dstat.py
@@ -15,12 +15,17 @@
 # - Add code to force cleanup of the environment for stale /var/run/ files.
 # - Incorporate better error handling and all around better file initiation of
 #   FIFO file and pid file.
+# - Clean up setup() and possibly rearrange.
+#   \- Specifically fix the try-except issue of a file not existing, but the
+#   directory doesn't have write permissions. Do so for both try-excepts.
+# - Move the /var/run files into a single directory in /var/run?
 ########################################################
 
 
 import os
 import sys
 import time
+import stat
 import getopt
 import psutil
 import subprocess
@@ -37,14 +42,40 @@ update_invl = 1
 status_msg_bool = False
 bar_width = 12
 
-basename=sys.argv[0:-3]
+# Strip ./, and .py from our basename
+bname = sys.argv[0]
+if bname[0] == '.' and bname[1] == '/':
+    basename = bname[2:-3]
+else:
+    basename = bname[0:-3]
+
 sleepd_ctl_file='/var/run/sleepd.ctl'
-run_file='/var/run/', basename, '.pid'
-sock_file='/var/run/', basename
+run_file='/tmp/%s.pid' % basename
+sock_file='/tmp/%s' % basename
 
 
 def cleanup():
-    None
+    try:
+        os.stat(run_file)
+    except OSError as err:
+        if err.errno == 2:
+            pass
+        else:
+            print("WARNING: %s: Could not delete PID file." % err.strerror)
+            pass
+    else:
+        os.unlink(run_file)
+
+    try:
+        os.stat(sock_file)
+    except OSError as err:
+        if err.errno == 2:
+            pass
+        else:
+            print("WARNING: %s: Could not delete FIFO file." % err.strerror)
+            pass
+    else:
+        os.unlink(sock_file)
 
 
 def cpu_avg(cpu_loads):
@@ -111,34 +142,34 @@ def help(msg=None):
 
 
 def main():
-	while True:
-	    cpu_perc = round(cpu_avg(psutil.cpu_percent(None, True)), 1)
-	    mem_perc = psutil.phymem_usage()[3]
-	
-	    cpu_bar = mk_prog_bar(cpu_perc)
-	    mem_bar = mk_prog_bar(mem_perc)
-	
-	    volume = get_volume()
-	
-	    stats = "Volume: " + str(volume) + " "
-	    stats += "CPU " + str(cpu_perc) + cpu_bar + " "
-	    stats += "MEM " + str(mem_perc) + mem_bar
-	  
-	    if status_msg_bool:
-	        stats = status_msg
-	        time.sleep(msg_ghost_time)
-	
-	    if stdout == True:
-	        print(stats)
-	        sys.exit(0)
-	
-	    else:
-	        ret = os.system("xsetroot -name '" + stats + "'")
-	        if ret > 0:
-	            print("ERROR: Binary 'xsetroot' is not installed.")
-	            sys.exit(255)
-	
-	    time.sleep(update_invl)
+    while True:
+        cpu_perc = round(cpu_avg(psutil.cpu_percent(None, True)), 1)
+        mem_perc = psutil.phymem_usage()[3]
+    
+        cpu_bar = mk_prog_bar(cpu_perc)
+        mem_bar = mk_prog_bar(mem_perc)
+    
+        volume = get_volume()
+    
+        stats = "Volume: " + str(volume) + " "
+        stats += "CPU " + str(cpu_perc) + cpu_bar + " "
+        stats += "MEM " + str(mem_perc) + mem_bar
+      
+        if status_msg_bool:
+            stats = status_msg
+            time.sleep(msg_ghost_time)
+    
+        if stdout == True:
+            print(stats)
+            sys.exit(0)
+    
+        else:
+            ret = os.system("xsetroot -name '" + stats + "'")
+            if ret > 0:
+                print("ERROR: Binary 'xsetroot' is not installed.")
+                sys.exit(255)
+    
+        time.sleep(update_invl)
 
 
 def mk_prog_bar(perc_val):
@@ -162,23 +193,47 @@ def setup():
     # Setup our PID file
     try:
         os.stat(run_file)
-    except OSError:
-        print("ERROR: Instance of ", basename, " is already running."
+        
+    except OSError as err: # run_file creation and errors
+        if err.errno == 2: # File doesn't exist
+            pid = os.getpid()
+            fd = os.open(run_file, os.O_CREAT | os.O_RDWR)
+            os.write(fd, "%d\n" % pid)
+            os.close(fd)
+            
+        else:
+            print("OSError: [Errno %d] %s: %s" % \
+                    (err.errno, err.strerror, run_file)) 
+            sys.exit(1)
+
+    else: # run_file exists
+        print("ERROR: Instance of %s is already running." % basename)
         print("  Was it closed cleanly?")
-        print("  See: \"ps aux | grep ", basename, ".py\"")
+        print("  See: \"ps aux | grep  %s.py\" Also: %s" % (basename, run_file))
         sys.exit(1)
 
-    pid = os.getpid()
-    # Create our PID file r/w, and truncate the data because we already
-    # check the environment for another running process
-    fd = os.open(run_file, os.O_RDWR | os.O_CREAT | os.O_TRUNC, 0660)
-    os.write(fd, str(pid, "\n"))
-    os.close(fd)
 
+    # Ensure our FIFO file exists
     try:
-        os.stat(sock_file)
-    except
+        ss = os.stat(sock_file)
+    
+    except OSError as err: 
+        if err.errno == 2: # sock_file doesn't exist
+            os.mkfifo(sock_file)
 
+        else: # other error
+            print("OSError: [%d] %s: %s" % (err.errno, err.strerror, sock_file))
+            sys.exit(1)
+
+    else:
+        # If our sock file is not a FIFO, blindly remove it
+        # and create the desired FIFO.
+        # TODO: Make this more polite.
+        if not stat.S_ISFIFO(ss.st_mode):
+            os.unlink(sock_file)
+            os.mkfifo(sock_file)
+
+    return 0 
     
 
 def sleep_enabled():
@@ -186,7 +241,8 @@ def sleep_enabled():
     try:
         os.stat(sleepd_ctl_file)
     except OSError:
-        print("ERROR: Sleepd does not exist. Is sleepd installed?\nNOTICE: Sleepd support disabled.")
+        print("ERROR: Sleepd does not exist. Is sleepd installed?\nNOTICE: \
+                Sleepd support disabled.")
 
     sleepd_fd = open(sleep_ctl_file, 4)
     if data[0:1] == 1:
@@ -196,37 +252,42 @@ def sleep_enabled():
 
 
 if __name__ == "__main__":
-	# Process our command line arguments
-	try:
-	    args, argv = getopt.getopt(sys.argv[1:], "hnsm:i:", ["help", "no-color", "stdout", "message=", "idle="])
-	except getopt.GetoptError:
-	    help("ERROR: Invalid argument supplied.")
-	    sys.exit(1)
-	
-	for arg, data in args:
-	    if arg in ("-h", "--help"):
-	        help()
-	
-	    elif arg in ("-n", "--no-color"):
-	        print("ERROR: No color, or code.")  
-	
-	    elif arg in ("-s", "--stdout"):
-	        stdout = True
-	
-	    elif arg in ("-m", "--message"):
-	        status_msg_bool = True
-	        status_msg = data
-	
-	    elif arg in ("-i", "--idle"):
-	        try:
-	            msg_ghost_time = int(data)
-	        except ValueError:
-	            help("ERROR: The ", arg, " argument expects and integar as a value. Char provided.")
-	
-	    else:
-	        help()
+    # Process our command line arguments
+    try:
+        args, argv = getopt.getopt(sys.argv[1:], "hnsm:i:", \
+                                    ["help", "no-color",    \
+                                    "stdout", "message=",   \
+                                    "idle="])
+    except getopt.GetoptError:
+        help("ERROR: Invalid argument supplied.")
+        sys.exit(1)
+    
+    for arg, data in args:
+        if arg in ("-h", "--help"):
+            help()
+    
+        elif arg in ("-n", "--no-color"):
+            print("ERROR: No color, or code.")  
+    
+        elif arg in ("-s", "--stdout"):
+            stdout = True
+    
+        elif arg in ("-m", "--message"):
+            status_msg_bool = True
+            status_msg = data
+    
+        elif arg in ("-i", "--idle"):
+            try:
+                msg_ghost_time = int(data)
+            except ValueError:
+                help("ERROR: The %s argument expects an integar as a value. \
+                    Char provided." % str(arg))
 
-
+        
+        else:
+            help()
+            
+    
     setup()
     main()
     cleanup()
